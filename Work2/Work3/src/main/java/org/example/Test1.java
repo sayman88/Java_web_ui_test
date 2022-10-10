7885b523436d7eb4366a3f0b923cf8914d822d1f
sayman88 <usurt428@yandex.ru>
2022-10-10 23:17:36 +0500

    Initial commit

---
+++ Work2/Work3/src/main/java/org/example/Test1.java
@@ -0,0 +1,39 @@
+
+package org.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
+
+public class Test1 {
+    public static void main(String[] args) {
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("start-maximized");
+        options.addArguments("--incognito");
+        options.addArguments("disable-popup-blocking");
+
+        WebDriver driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        // обработка предусловия
+        driver.get("https://ribomaniya.ru/?logout=yes");
+
+        // тестовые действия
+        driver.get("https://ribomaniya.ru/cabinet/auth/?login=yes&backurl=%2F");
+        driver.findElement(By.xpath("//input[@name='USER_LOGIN']")).sendKeys("stendMerlin");
+        driver.findElement(By.xpath("//input[@name='USER_PASSWORD']")).sendKeys("D2EA_7abd");
+        driver.findElement(By.xpath("//button[@name='Login']")).click();
+        String s = driver.findElement(By.xpath("//a[@class='nav-link text-truncate pt-0']")).getText();
+        assert (s.equals("Александр"));
+
+        //результат теста
+        System.out.println("Тест №1 пройден");   // выведется только если тест не упадет и условия удовлетворят assert
+
+        // выход из браузера
+        driver.quit();
+    }
+}
